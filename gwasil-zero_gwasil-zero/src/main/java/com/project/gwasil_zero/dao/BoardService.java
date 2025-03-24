c15a7445eb3c522ad81559280d71d239239b7d42
github <github>
2025-03-24 17:08:31 +0900

    boardadd

---
+++ gwasil-zero_gwasil-zero/src/main/java/com/project/gwasil_zero/dao/BoardService.java
@@ -0,0 +1,58 @@
+package com.project.gwasil_zero.dao;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project.gwasil_zero.mapper.BoardMapper;
+
+@Service
+public class BoardService {
+	@Autowired
+	BoardMapper boardMapper;
+	
+	public HashMap<String, Object> saveBoard(HashMap<String, Object> map) {
+	    HashMap<String, Object> resultMap = new HashMap<>();
+	    try {
+	        boardMapper.insertBoard(map); // boardNo는 이 map 안에 들어옴
+	        System.out.println("Inserted Board Map: " + map);
+
+	        // boardNo를 BigDecimal로 받고 int로 변환
+	        Object boardNoObj = map.get("boardNo");
+	        int boardNo = 0;
+
+	        if (boardNoObj instanceof BigDecimal) {
+	            boardNo = ((BigDecimal) boardNoObj).intValue();
+	        } else if (boardNoObj instanceof Integer) {
+	            boardNo = (Integer) boardNoObj;
+	        }
+
+	        resultMap.put("result", "success");
+	        resultMap.put("boardNo", boardNo);
+
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        resultMap.put("result", "failed");
+	    }
+
+	    return resultMap;
+	}
+
+	
+	
+	public HashMap<String, Object> saveBoardFile(HashMap<String, Object> map) {
+		HashMap<String, Object> resultMap = new HashMap<String, Object>();
+		try {
+			boardMapper.insertBoardFile(map);
+			System.out.println(map);
+			resultMap.put("fileResult","success");
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+			resultMap.put("fileResult","failed");
+		}
+		
+		return resultMap;
+	}
+}

631042937e8bb0a438d5df3e9cce6fdf3679aa3b
Yeonju02 <cyj32148@naver.com>
2025-03-24 10:36:00 +0900

    25.03.24 package setting

---
+++ gwasil-zero_gwasil-zero/src/main/java/com/project/gwasil_zero/model/User.java
@@ -0,0 +1,15 @@
+package com.project.gwasil_zero.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+	
+	private String userName;
+	private String userPwd;
+	private String userStatus;
+	private String userPhone;
+	private String userId;
+	private int reportCnt;
+
+}

881e2bc600f816b48dc37accaa1c021ec4186d79
Yeonju02 <cyj32148@naver.com>
2025-04-02 13:12:19 +0900

    25.04.02 merge 하기 전 model, header 수정

--- gwasil-zero_gwasil-zero/src/main/java/com/project/gwasil_zero/model/Contract.java
+++ gwasil-zero_gwasil-zero/src/main/java/com/project/gwasil_zero/model/Contract.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class Contract {
+	
    private int contractNo;
    private String userId;
    private String lawyer_Id;
@@ -12,4 +13,5 @@ public class Contract {
    private String refundAccuont;
    private String cdate;
    private String contractStatus;
+   
 }

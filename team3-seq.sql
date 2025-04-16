788496879f8fe3085297831c1c8cbe1e9394072f
SaeByeol5285 <sebyulpark@naver.com>
2025-04-16 14:53:31 +0900

    프로젝트 DB

---
+++ team3-seq.sql
@@ -0,0 +1,93 @@
+--------------------------------------------------------
+--  파일이 생성됨 - 수요일-4월-16-2025   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Sequence BOARD_CMT_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SYSTEM"."BOARD_CMT_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 261 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence BOARD_FILE_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SYSTEM"."BOARD_FILE_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 3 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence BOARD_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SYSTEM"."BOARD_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 292 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence BOOKMARK_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SYSTEM"."BOOKMARK_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 281 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence CATEGORIES_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SYSTEM"."CATEGORIES_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence CHAT_FILE_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SYSTEM"."CHAT_FILE_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence CHAT_MESSAGE_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SYSTEM"."CHAT_MESSAGE_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence CHAT_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SYSTEM"."CHAT_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 141 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence CONTRACT_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SYSTEM"."CONTRACT_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 29 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence LICENSE_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SYSTEM"."LICENSE_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence NOTIFICATION_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SYSTEM"."NOTIFICATION_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 841 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence PACKAGE_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SYSTEM"."PACKAGE_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence PAY_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SYSTEM"."PAY_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence REPORT_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SYSTEM"."REPORT_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 36 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence REVIEW_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SYSTEM"."REVIEW_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 161 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence TOTAL_CMT_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SYSTEM"."TOTAL_CMT_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 141 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence TOTAL_FILE_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SYSTEM"."TOTAL_FILE_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence TOTALDOCS_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SYSTEM"."TOTALDOCS_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 261 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
